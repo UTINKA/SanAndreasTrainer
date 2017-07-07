1ef9ec6dfb4d2ebfca5b6a191122479eff41aa30
Chris Caldwell <ccaldwell488@gmail.com>
2017-07-08 00:16:03 +0100

    initial commit

---
+++ solSanAndreasTrainer/projSanAndreasTrainer/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace projSanAndreasTrainer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnAttach = new System.Windows.Forms.Button();
+            this.lblX = new System.Windows.Forms.Label();
+            this.tmrProcess = new System.Windows.Forms.Timer(this.components);
+            this.lblY = new System.Windows.Forms.Label();
+            this.lblZ = new System.Windows.Forms.Label();
+            this.lblHealth = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnAttach
+            // 
+            this.btnAttach.Location = new System.Drawing.Point(90, 11);
+            this.btnAttach.Name = "btnAttach";
+            this.btnAttach.Size = new System.Drawing.Size(75, 23);
+            this.btnAttach.TabIndex = 0;
+            this.btnAttach.Text = "Attach";
+            this.btnAttach.UseVisualStyleBackColor = true;
+            this.btnAttach.Click += new System.EventHandler(this.btnAttach_Click);
+            // 
+            // lblX
+            // 
+            this.lblX.AutoSize = true;
+            this.lblX.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblX.Location = new System.Drawing.Point(43, 47);
+            this.lblX.Name = "lblX";
+            this.lblX.Size = new System.Drawing.Size(16, 20);
+            this.lblX.TabIndex = 1;
+            this.lblX.Text = "x";
+            // 
+            // tmrProcess
+            // 
+            this.tmrProcess.Enabled = true;
+            this.tmrProcess.Tick += new System.EventHandler(this.tmrProcess_Tick);
+            // 
+            // lblY
+            // 
+            this.lblY.AutoSize = true;
+            this.lblY.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblY.Location = new System.Drawing.Point(43, 107);
+            this.lblY.Name = "lblY";
+            this.lblY.Size = new System.Drawing.Size(16, 20);
+            this.lblY.TabIndex = 2;
+            this.lblY.Text = "y";
+            // 
+            // lblZ
+            // 
+            this.lblZ.AutoSize = true;
+            this.lblZ.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblZ.Location = new System.Drawing.Point(43, 77);
+            this.lblZ.Name = "lblZ";
+            this.lblZ.Size = new System.Drawing.Size(17, 20);
+            this.lblZ.TabIndex = 3;
+            this.lblZ.Text = "z";
+            // 
+            // lblHealth
+            // 
+            this.lblHealth.AutoSize = true;
+            this.lblHealth.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHealth.Location = new System.Drawing.Point(43, 137);
+            this.lblHealth.Name = "lblHealth";
+            this.lblHealth.Size = new System.Drawing.Size(31, 20);
+            this.lblHealth.TabIndex = 4;
+            this.lblHealth.Text = "HP";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(2, 137);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 20);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "HP";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(2, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "z";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(2, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(16, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "y";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(2, 47);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(16, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "x";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(255, 167);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblHealth);
+            this.Controls.Add(this.lblZ);
+            this.Controls.Add(this.lblY);
+            this.Controls.Add(this.lblX);
+            this.Controls.Add(this.btnAttach);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "GTA SA Trainer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAttach;
+        private System.Windows.Forms.Label lblX;
+        private System.Windows.Forms.Timer tmrProcess;
+        private System.Windows.Forms.Label lblY;
+        private System.Windows.Forms.Label lblZ;
+        private System.Windows.Forms.Label lblHealth;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ solSanAndreasTrainer/projSanAndreasTrainer/Form1.cs
@@ -0,0 +1,113 @@
+﻿using ProcessMemoryReaderLib;
+using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace projSanAndreasTrainer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Process[] SA;
+        ProcessModule mainModule;
+        ProcessMemoryReader mem = new ProcessMemoryReader();
+
+        struct offsets
+        {
+            static public int health, baseAddr;
+            static public int[] xPos, zPos, yPos;
+            // the game which uses XZY and not XYZ
+        }
+
+        bool gameFound = false;
+
+        float[,] teleCoords = new float[2 , 3];
+
+        private void btnAttach_Click(object sender , EventArgs e)
+        {
+            try
+            {
+                SA = Process.GetProcessesByName("gta_sa");
+                mainModule = SA[0].MainModule;
+                mem.ReadProcess = SA[0];
+                mem.OpenProcess();
+                gameFound = true;
+
+                offsets.baseAddr = 0xB6F5F0;
+                offsets.health = 0x540;
+                offsets.xPos = new int[] { 0x14 , 0x30 };
+                offsets.zPos = new int[] { 0x14 , 0x34 };
+                offsets.yPos = new int[] { 0x14 , 0x38 };
+
+                btnAttach.BackColor = Color.Green; //User Feedback
+                btnAttach.Enabled = false;
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                MessageBox.Show("Game not found!");
+                //throw ex;
+            }
+        }
+
+        private void tmrProcess_Tick(object sender , EventArgs e)
+        {
+            if (gameFound && !SA[0].HasExited)
+            {
+                // Only the base address or multiLevel addresses need to be established first
+                int playerBaseAddress = mem.ReadInt(offsets.baseAddr);
+                // Stores an address which is the base address of the player struct.
+                int XAddress = mem.ReadMultiLevelPointer(offsets.baseAddr , 4 , offsets.xPos);
+                // Multi level pointer with 2 offsets is needed to find the address of X position,
+                // then this address can be read as a float.
+                int ZAddress = mem.ReadMultiLevelPointer(offsets.baseAddr , 4 , offsets.zPos);
+                int YAddress = mem.ReadMultiLevelPointer(offsets.baseAddr , 4 , offsets.yPos);
+
+
+                lblX.Text = mem.ReadFloat(XAddress).ToString();
+                lblZ.Text = mem.ReadFloat(ZAddress).ToString();
+                lblY.Text = mem.ReadFloat(YAddress).ToString();
+
+                lblHealth.Text = mem.ReadFloat(playerBaseAddress + offsets.health).ToString();
+
+                int Hotkey = ProcessMemoryReaderApi.GetKeyState(0x74);//F5
+                if ((Hotkey & 0x8000) != 0)
+                {
+                    // Teleport to Grove Street
+                    mem.WriteFloat(XAddress , 2495);
+                    mem.WriteFloat(ZAddress , -1668);
+                    mem.WriteFloat(YAddress , 13);
+                }
+
+                int Hotkey2 = ProcessMemoryReaderApi.GetKeyState(0x75);//F6
+                if ((Hotkey2 & 0x8000) != 0)
+                {
+                    // Teleport to Dome Stadium Roof
+                    mem.WriteFloat(XAddress , 2737);
+                    mem.WriteFloat(ZAddress , -1760);
+                    mem.WriteFloat(YAddress , 44);
+                }
+
+                int Hotkey3 = ProcessMemoryReaderApi.GetKeyState(0x76);//F7
+                if ((Hotkey3 & 0x8000) != 0)
+                {
+                    // Teleport to Skyscraper
+                    mem.WriteFloat(XAddress , 1544);
+                    mem.WriteFloat(ZAddress , -1353);
+                    mem.WriteFloat(YAddress , 330);
+                }
+            }
+            else
+            {
+                // Game has ended so stop performing readMemory etc
+                gameFound = false;
+                btnAttach.BackColor = Color.Red;
+                btnAttach.Enabled = true;
+            }
+        }//tmrProcess
+    }
+}
